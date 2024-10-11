b871be8c22f24d6ffbee969983c93981aedf570d
Thar Htet San <tharhtet1234@gmail.com>
2024-10-11 21:57:10 +0700

    add

--- 9_ML_System_Monitoring/monitoring_with_evidentlyai/prefect_monitoring.py
+++ 9_ML_System_Monitoring/monitoring_with_evidentlyai/prefect_monitoring.py
@@ -7,7 +7,7 @@ from evidently import ColumnMapping
 from evidently.metrics import ColumnDriftMetric, DatasetDriftMetric, DatasetMissingValuesMetric
 import random
 import logging
-
+import time
 
 
 
@@ -17,7 +17,7 @@ reference_data = pd.read_parquet('data/reference.parquet')
 with open('models/lin_reg.bin', 'rb') as f_in:
 	model = joblib.load(f_in)
 
-raw_data = pd.read_parquet('data/original_data-02.parquet')
+raw_data = pd.read_parquet('data/original_data.parquet')
 
 print("raw_data : ",raw_data.shape)
 print("reference_data : ",reference_data.shape)
@@ -66,10 +66,27 @@ def prep_db():
 		with psycopg.connect("host=localhost port=5432 dbname=test_db user=postgres password=tharhtet") as conn:
 			conn.execute(create_table_statement)
 
-def calculate_dummy_metrics_postgresql(curr):
-	i = 0
+def calculate_dummy_metrics_postgresql(curr,i):
 	current_data = raw_data[(raw_data.lpep_pickup_datetime >= (begin + datetime.timedelta(i))) &
 		(raw_data.lpep_pickup_datetime < (begin + datetime.timedelta(i + 1)))]	
+	report.run(reference_data = reference_data, current_data = current_data,
+		column_mapping=column_mapping)
+
+	result = report.as_dict()
+
+	prediction_drift = result['metrics'][0]['result']['drift_score']
+	num_drifted_columns = result['metrics'][1]['result']['number_of_drifted_columns']
+	share_missing_values = result['metrics'][2]['result']['current']['share_of_missing_values']
+
+	print(i)
+	print(prediction_drift)
+
+	curr.execute(
+		"insert into dummy_metrics(timestamp, prediction_drift, num_drifted_columns, share_missing_values) values (%s, %s, %s, %s)",
+		(begin + datetime.timedelta(i), prediction_drift, num_drifted_columns, share_missing_values)
+	)
+
+
 
 
 def batch_monitoring_backfill():
@@ -77,6 +94,16 @@ def batch_monitoring_backfill():
 	last_send = datetime.datetime.now() - datetime.timedelta(seconds=10)
 	with psycopg.connect("host=localhost port=5432 dbname=test_db user=postgres password=tharhtet", autocommit=True) as conn:
 		for i in range(0, 27):
-		
-			#with conn.cursor() as curr:
-			#	calculate_metrics_postgresql(curr, i)
+			with conn.cursor() as curr:
+				calculate_dummy_metrics_postgresql(curr,i)
+			
+			
+			new_send = datetime.datetime.now()
+			seconds_elapsed = (new_send - last_send).total_seconds()
+			if seconds_elapsed < SEND_TIMEOUT:
+				time.sleep(SEND_TIMEOUT - seconds_elapsed)
+			while last_send < new_send:
+				last_send = last_send + datetime.timedelta(seconds=10)
+			logging.info("data sent")
+
+batch_monitoring_backfill()
